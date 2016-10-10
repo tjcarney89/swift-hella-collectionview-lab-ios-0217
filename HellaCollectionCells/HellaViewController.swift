7b409aa20774c8ce52c9273af111d5b6239f555f
ianrahman <ian.alexander.rahman@gmail.com>
2016-10-10 10:02:57 -0400

    Working on solution

--- HellaCollectionCells/HellaViewController.swift
+++ HellaCollectionCells/HellaViewController.swift
@@ -8,15 +8,47 @@
 
 import UIKit
 
-class HellaViewController: UIViewController {
-
+class HellaViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var fibonacciSequence: [Int] {
+        var arr: [Int] = []
+        var i = 1
+        while fibonacci(i) <= 1000 {
+            arr.append(fibonacci(i))
+            i += 1
+        }
+        return arr
+    }
+    
+    @IBOutlet weak var hellaCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
     // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1000
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "hellaCell", for: indexPath)
+        
+        cell.backgroundColor = UIColor.yellow
+        
+        if fibonacciSequence.contains(indexPath.row) {
+            cell.backgroundColor = UIColor.purple
+        }
+        
+        return cell
+    }
+    
+    func fibonacci(_ i: Int) -> Int {
+        if i <= 2 {
+            return 1
+        } else {
+            return fibonacci(i - 1) + fibonacci(i - 2)
+        }
     }
 }
 
